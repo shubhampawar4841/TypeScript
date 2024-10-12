30589bea1871711e197e7722174ed48fe20fb543
shubhampawar4841 <shubhampawar4036@gmail.com>
2024-10-12 10:09:04 +0530

    types

---
+++ node-app/a.js
@@ -0,0 +1,7 @@
+"use strict";
+let x = 1001;
+console.log(x);
+function greeting(Name) {
+    console.log("Hello" + Name);
+}
+greeting("shubham");
---
+++ node-app/a.ts
@@ -0,0 +1,10 @@
+
+
+let x:number =1001;
+
+console.log(x);
+
+function greeting(Name:string) {
+    console.log("Hello"+ Name);
+}
+greeting("shubham");
