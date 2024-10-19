c4d73b661014b653eafa70620b9a24d5f3f5d1f7
Shubham Pawar <114252697+shubhampawar4841@users.noreply.github.com>
2024-10-19 23:07:40 +0530

    Update index.js

--- node-app/index.js
+++ node-app/index.js
@@ -7,3 +7,13 @@ setTimeout(greet, 1*1000);
 function setInterval(() => {
     
 }, interval);
+
+function greet() {
+    console.log("Hellow");
+}
+
+setTimeout(greet, 1*1000);
+
+function setInterval(() => {
+    
+}, interval);
