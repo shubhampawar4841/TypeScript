e4da08f6661829e24d731941595e2a420c8786d5
Shubham Pawar <114252697+shubhampawar4841@users.noreply.github.com>
2024-10-19 23:08:51 +0530

    Update index.js

--- node-app/index.js
+++ node-app/index.js
@@ -12,8 +12,19 @@ function greet() {
     console.log("Hellow");
 }
 
-setTimeout(greet, 1*1000);
+setTimeout(greet, 3*1000);
 
 function setInterval(() => {
     
 }, interval);
+
+function greet() {
+    console.log("Great");
+}
+
+setTimeout(greet, 7*1000);
+
+function setInterval(() => {
+    
+}, interval);
+
